0e4b0afca9031f172fc8a23ae8228b018d219dc1
Carlos Rodriguez <carlosgrodriguezl@gmail.com>
2019-12-19 12:06:23 +0100

    create form, no upload, no validation

--- visjo-frontend/src/main/angular/src/app/create/create.component.ts
+++ visjo-frontend/src/main/angular/src/app/create/create.component.ts
@@ -1,15 +1,97 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import * as EXIF from 'exif-js/exif';
+import {isArray} from 'util';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.sass']
 })
-export class CreateComponent implements OnInit {
+export class CreateComponent {
+    public journeyForm: FormGroup;
+    public files: File[] = [];
 
-  constructor() { }
+    public images: string[] = [];
 
-  ngOnInit() {
-  }
+    public arrayImages: {
+        title: string;
+        lat: number;
+        long: number;
+        date: string;
+        file: File;
+    }[] = [];
 
+    constructor(private formBuilder: FormBuilder) {
+        this.journeyForm = this.formBuilder.group({
+            title: [],
+            images: this.formBuilder.array([])
+        });
+    }
+
+    getEXIFData(file) {
+        return new Promise(function(resolve) {
+            EXIF.getData(file, function() {
+                EXIF.pretty(this);
+
+                let latitude = this.exifdata.GPSLatitude;
+                let longitude = this.exifdata.GPSLongitude;
+
+                if (!latitude || !longitude) {
+                    return { lat: 0, long: 0 };
+                }
+                if (isArray(latitude)) {
+                    latitude = latitude[latitude.length - 1];
+                }
+                if (isArray(longitude)) {
+                    longitude = longitude[longitude.length - 1];
+                }
+                resolve([ latitude || 0, longitude || 0 ]);
+            });
+        });
+    }
+
+    addFileToBeDisplayed(file) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            if (typeof reader.result === 'string') {
+                this.images.push(reader.result);
+            }
+        };
+        reader.readAsDataURL(file);
+    }
+
+    async onFileChanged(event) {
+        for (let file of event.target.files) {
+            if (!file || !file.name) {
+                return;
+            }
+            file = file as File;
+            this.files.push(file);
+            const title = file.name;
+            const date = new Date(file.lastModified).toISOString();
+            let result = await this.getEXIFData(file);
+            this.addFileToBeDisplayed(file);
+            if (!result) {
+                result = [0, 0];
+            }
+            const lat = result[0];
+            const long = result[1];
+            this.arrayImages.push({
+                title,
+                lat,
+                long,
+                date,
+                file
+            });
+        }
+    }
+
+    onUpload() {
+        // upload code goes here
+    }
+
+    submit() {
+        console.log(this.journeyForm.value);
+    }
 }
