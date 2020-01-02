1e48fb90c859d459a686cf87a08970056a74b7b7
DHeger <e1026721@student.tuwien.ac.at>
2020-01-02 17:27:53 +0100

    fixed typo lol

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,7 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" || exit
 cd "${DIR}/.." || exit  # go to project root
 
 REPO_PATH="$(pwd)"
-echo REPO_PATH
+echo $REPO_PATH
 export REPO_PATH
 
 docker-compose up -d
