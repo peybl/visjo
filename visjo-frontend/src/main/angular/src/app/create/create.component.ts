5b818138f7985d6d915a262b0b71cbf80011c36d
Carlos Rodriguez <carlosgrodriguezl@gmail.com>
2020-01-03 15:22:36 +0100

    edit page save function

--- visjo-frontend/src/main/angular/src/app/create/create.component.ts
+++ visjo-frontend/src/main/angular/src/app/create/create.component.ts
@@ -2,6 +2,9 @@ import {Component} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import * as EXIF from 'exif-js/exif';
 import {isArray} from 'util';
+import {Journey} from '../dtos/Journey';
+import {Image} from '../dtos/Image';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-create',
@@ -10,17 +13,10 @@ import {isArray} from 'util';
 })
 export class CreateComponent {
     public journeyForm: FormGroup;
-    public files: File[] = [];
+    public journey: Journey = new Journey();
 
-    public images: string[] = [];
-
-    public arrayImages: {
-        title: string;
-        lat: number;
-        long: number;
-        date: string;
-        file: File;
-    }[] = [];
+    public images: Image[] = [];
+    public newFiles: File[] = [];
 
     constructor(private formBuilder: FormBuilder) {
         this.journeyForm = this.formBuilder.group({
@@ -51,14 +47,32 @@ export class CreateComponent {
         });
     }
 
-    addFileToBeDisplayed(file) {
-        const reader = new FileReader();
-        reader.onload = (e) => {
-            if (typeof reader.result === 'string') {
-                this.images.push(reader.result);
-            }
-        };
-        reader.readAsDataURL(file);
+    getFileLink(file) {
+        return new Promise(function(resolve) {
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                if (typeof reader.result === 'string') {
+                    resolve(reader.result);
+                }
+            };
+            reader.readAsDataURL(file);
+        });
+    }
+
+    updateForm() {
+        const formdata = this.journeyForm.getRawValue();
+        this.journey.name = formdata.title;
+
+        const myArray = $('.image');
+
+        for (let i = 0; i < myArray.length; i++) {
+            this.images[i].name = $('#title-' + i).val();
+            this.images[i].longitude = $('#long-' + i).val();
+            this.images[i].latitude = $('#lat-' + i).val();
+            this.images[i].date = new Date($('#date-' + i).val());
+        }
+        this.journey.images = this.images;
+        debugger;
     }
 
     async onFileChanged(event) {
@@ -67,28 +81,83 @@ export class CreateComponent {
                 return;
             }
             file = file as File;
-            this.files.push(file);
-            const title = file.name;
-            const date = new Date(file.lastModified).toISOString();
+            this.newFiles.push(file);
+            const image = new Image();
+            image.name = file.name;
+            image.date = new Date(file.lastModified);
             let result = await this.getEXIFData(file);
-            this.addFileToBeDisplayed(file);
             if (!result) {
                 result = [0, 0];
             }
-            const lat = result[0];
-            const long = result[1];
-            this.arrayImages.push({
-                title,
-                lat,
-                long,
-                date,
-                file
+            image.latitude = result[0];
+            image.longitude = result[1];
+            const uri = await this.getFileLink(file);
+            if (typeof uri === 'string') {
+                image.source = uri;
+            }
+            this.images.push(image);
+        }
+    }
+
+    preUpload(journey) {
+        return new Promise(function(resolve) {
+            $.post('/journey', journey)
+                .done((result) => {
+                    console.log(result);
+                    resolve({success: true, result});
+                })
+                .fail((err) => {
+                    console.error('Error creating journey with name "' + $(this).name.value + '"', err);
+                    resolve({success: false});
+                });
+        });
+    }
+
+    async onUpload() {
+        this.updateForm();
+        let journey = await this.preUpload(this.journey);
+        for (const image of this.images) {
+            const data = new FormData();
+            data.append('file', this.newFiles[1], image.name);
+            data.append('journeyId', this.journey.id);
+            data.append('latitude', image.latitude + '');
+            data.append('longitude', image.longitude + '');
+            data.append('timestamp', this.getUtcString());
+            debugger;
+
+            $.ajax({
+                url: '/image',
+                method: 'POST',
+                enctype: 'multipart/form-data',
+                processData: false,
+                contentType: false,
+                cache: false,
+                data
+            }).done((image) => {
+                console.log(image);
+            }).fail((err) => {
+                console.error('Error uploading file "' + image.name + '"', err.responseText);
             });
         }
     }
 
-    onUpload() {
-        // upload code goes here
+    getUtcString() {
+        const now = new Date();
+        const utc = [];
+        utc.push(now.getFullYear());
+        utc.push('-');
+        utc.push(('0' + (now.getMonth() + 1)).slice(-2));
+        utc.push('-');
+        utc.push(('0' + now.getDate()).slice(-2));
+        utc.push('T');
+        utc.push(('0' + now.getHours()).slice(-2));
+        utc.push(':');
+        utc.push(('0' + now.getMinutes()).slice(-2));
+        utc.push(':');
+        utc.push(('0' + now.getSeconds()).slice(-2));
+        utc.push('+');
+        utc.push(('0' + now.getTimezoneOffset() / -60).slice(-2));
+        return utc.join('');
     }
 
     submit() {
